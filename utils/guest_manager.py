c9bab9cfc5b8af8843a7a900cdbe04a83e7bd133
devquint <ksmin1114@g.skku.edu>
2023-07-31 16:35:36 +0900

    Update guest_manager.py

--- utils/guest_manager.py
+++ utils/guest_manager.py
@@ -21,13 +21,13 @@ class GuestManager:
 
         # Get the current transmitted gbits
         self.last_time = datetime.now()
-        self.last_bytes = guest_utils.check_tx_bytes(self.name, self.iface)
+        self.last_bytes = guest_utils.check_tx_gbits(self.name, self.iface)
 
         if last_bytes == -1:
             return -1
         
-        time_diff = (self.last_time - last_time).microseconds
+        time_diff = (self.last_time - last_time).seconds
         bytes_diff = self.last_bytes - last_bytes
-        return float(bytes_diff) / 2.0**(20-3) / (float(time_diff) * 1000000.0)
+        return float(bytes_diff) / float(time_diff)
     
         
